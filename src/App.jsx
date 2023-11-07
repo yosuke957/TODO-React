40fd762fce4876d7dc7079b2fbd805558c3b6d50
yosuke957 <140671319+yosuke957@users.noreply.github.com>
2023-11-07 11:17:15 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
+
+export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  // 追加ボタン押下時の処理
+  const onClickAdd = () => {
+    // 処理が一行のため、{}はなくても問題ない。
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  };
+
+  // 削除ボタン押下時の処理
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // 何番目の要素から何個削除するか
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  // 完了ボタン押下時の処理
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    // 何番目の要素から何個削除するか
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  // 戻るボタン押下時の処理
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
+      />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          登録できるtodoは５個までだよ〜。消化しろ〜。
+        </p>
+      )}
+
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
+};
---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+// 下記のようにclass nameを設定し、cssファイルに記載するのではなく、
+// コンポーネントファイルの中に記載する方法もある
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px"
+};
+
+export const InputTodo = (props) => {
+  const { todoText, onChange, onClick, disabled } = props;
+  return (
+    <div style={style}>
+      <input
+        disabled={disabled}
+        placeholder="TODO入力"
+        value={todoText}
+        onChange={onChange}
+      />
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
+    </div>
+  );
+};
